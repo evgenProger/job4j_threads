cc1fe8abf8404b98ed8647fd8044ef100e30c50d
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-19 18:11:30 +0500

    Demo State Threads

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                }
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " " + first.getState());
+            System.out.println(second.getName() + " " + second.getState());
+        }
+        System.out.println("The Threads first and second have finished their work");
+    }
+}
