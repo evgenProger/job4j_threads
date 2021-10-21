c773e4df10ad98bd2e601e27d860caad51168e22
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-21 20:59:26 +0500

    Download file with limit time.

--- src/main/java/ru/job4j/concurrent/FileDownload.java
+++ src/main/java/ru/job4j/concurrent/FileDownload.java
@@ -1,5 +1,9 @@
 package ru.job4j.concurrent;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.net.URL;
+
 public class FileDownload implements Runnable {
     private final String url;
     private final int speed;
@@ -11,6 +15,33 @@ public class FileDownload implements Runnable {
 
     @Override
     public void run() {
-
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream out = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                out.write(dataBuffer, 0, 1024);
+                long finish = System.currentTimeMillis();
+                long differentTime = finish - start;
+                start = System.currentTimeMillis();
+                if (speed > differentTime) {
+                    Thread.sleep(speed - differentTime);
+                }
+            }
+        } catch (Exception e) {
+           Thread.currentThread().interrupt();
     }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread download = new Thread(new FileDownload(url, speed));
+        download.start();
+        download.join();
+    }
+
+
 }
+
