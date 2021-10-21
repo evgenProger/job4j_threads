82867f4646384521bbbbc76733ca22213d7cc015
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-21 13:43:11 +0500

    Demo interrupt of Thread

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -3,20 +3,26 @@ package ru.job4j.concurrent;
 public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
-        String[] symbols = new String[] {"\\", "|", "/"};
-        while (!Thread.currentThread().isInterrupted()) {
-            for (String str: symbols) {
-                System.out.print("\rLoad :" + " " + str);
+        int index = 0;
+        String[] symbols = new String[]{"\\", "|", "/"};
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(500);
+                System.out.print("\r Load: " + " " + symbols[index]);
+                index++;
+                if (index > symbols.length - 1) {
+                    index = 0;
+                }
             }
-
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         progress.interrupt();
-
     }
 }
