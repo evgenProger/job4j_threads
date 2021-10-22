ac75da35b80ee8010b9d46d37048e9d8d859cb93
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-10-22 11:56:21 +0500

    Download file with limit time.

--- src/main/java/ru/job4j/concurrent/FileDownload.java
+++ src/main/java/ru/job4j/concurrent/FileDownload.java
@@ -7,16 +7,18 @@ import java.net.URL;
 public class FileDownload implements Runnable {
     private final String url;
     private final int speed;
+    private final String target;
 
-    public FileDownload(String url, int speed) {
+    public FileDownload(String url, int speed, String target) {
         this.url = url;
         this.speed = speed;
+        this.target = target;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream out = new FileOutputStream(target)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             long start = System.currentTimeMillis();
@@ -30,18 +32,21 @@ public class FileDownload implements Runnable {
                 }
             }
         } catch (Exception e) {
-           Thread.currentThread().interrupt();
-    }
+            Thread.currentThread().interrupt();
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("No arguments found");
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread download = new Thread(new FileDownload(url, speed));
+        String target = args[2];
+
+        Thread download = new Thread(new FileDownload(url, speed, target));
         download.start();
         download.join();
     }
-
-
 }
 
