ca2e82f58432db9c96e8c071daacdc1ceed079b3
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-11-01 17:46:54 +0500

    The program blocks execution of the counter conditionally.

---
+++ src/main/java/ru/job4j/waitnotify/CountBarrier.java
@@ -0,0 +1,31 @@
+package ru.job4j.waitnotify;
+
+/**
+ * Класс, блокирует выполнение счетчика по условию.
+ */
+
+public class CountBarrier {
+
+    private final Object monitor = this;
+    private final int total;
+    private int count = 0;
+
+    public CountBarrier(int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+        }
+    }
+
+    public void await() throws InterruptedException {
+        synchronized (monitor) {
+            while (!(count >= total)) {
+                monitor.wait();
+            }
+        }
+    }
+}
