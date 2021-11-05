24ec383492708783f89fb84e3d58859ac604c0b9
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-11-05 22:43:18 +0500

    Non Blocking Counter

---
+++ src/main/java/ru/job4j/nonblockingalgoritm/CASCount.java
@@ -0,0 +1,22 @@
+package ru.job4j.nonblockingalgoritm;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CASCount {
+    private int number = 1;
+    private final AtomicReference<Integer> count = new AtomicReference<>(number);
+
+    public void increment() {
+        if(!(count.compareAndSet(number, number++))) {
+            throw new UnsupportedOperationException("Count is not impl.");
+        }
+    }
+
+    public int get() {
+        int result = count.get();
+        if (result == number) {
+            throw new UnsupportedOperationException("Count is not impl.");
+        }
+        return result;
+    }
+}
