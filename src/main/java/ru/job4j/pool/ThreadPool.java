ac4469cad404f9a26db88a1a66c6fbfbc978f5e1
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-11-13 18:36:10 +0500

    Thread Pool Demo.

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -9,15 +9,32 @@ import java.util.concurrent.*;
 
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(5);
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(5);;
+
+    public ThreadPool() {
+        int size = Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    tasks.poll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            threads.add(thread);
+        }
+    }
 
     public void work(Runnable job) throws InterruptedException {
         tasks.offer(job);
     }
 
     public void shutdown() throws InterruptedException {
-        while (!tasks.isEmpty()) {
-            threads.add((Thread) tasks.poll());
+        for (Thread t: threads) {
+           while (!t.isInterrupted()) {
+               t.start();
+               t.interrupt();
+           }
         }
     }
 }
