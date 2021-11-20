6f98f95ebf84fb71b9708b9d054160989638ea6b
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2021-11-20 18:14:31 +0500

    Email service. Use Thread pool.

--- src/main/java/ru/job4j/pool/emailservice/EmailNotification.java
+++ src/main/java/ru/job4j/pool/emailservice/EmailNotification.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
 
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
@@ -20,6 +20,13 @@ public class EmailNotification {
 
     public void close() {
        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
